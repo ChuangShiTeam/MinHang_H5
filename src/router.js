58ac10f7c2136cd4be50a3f3627d523f977e8db3
lirongzhi@qianjiangcloud.com <lirongzhi@qianjiangcloud.com>
2017-10-02 17:43:18 +0800

    new01 02两个页面

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Router, Route, IndexRedirect} from "dva/router";
 import Main from "./view/Main";
 import Index00 from "./view/00/Index";
+import Index01 from "./view/01/Index";
+import Index02 from "./view/02/Index";
 
 import constant from "./util/constant";
 
@@ -13,6 +15,8 @@ function RouterConfig({history}) {
                 <IndexRedirect to={constant.index}/>
                 <Route component={Main}>
                     <Route path="/00/index" component={Index00}/>
+                    <Route path="/01/index" component={Index01}/>
+                    <Route path="/02/index" component={Index02}/>
                 </Route>
             </Route>
         </Router>
