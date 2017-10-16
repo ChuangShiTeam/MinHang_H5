738985eed65533472db9599fefff8ac16908543a
marcus <marcus_hualebin@163.com>
2017-10-16 08:27:00 +0800

    完善

--- src/view/key2/Index.js
+++ src/view/key2/Index.js
@@ -20,6 +20,7 @@ import Record from '../Record';
 import constant from '../../util/constant';
 import http from '../../util/http';
 import notification from '../../util/notification';
+import Picture from '../Picture';
 
 class Index extends Component {
     constructor(props) {
@@ -56,12 +57,37 @@ class Index extends Component {
                 }
             });
         });
+
+        notification.on('notification_2_picture', this, function (data) {
+            window.wx.chooseImage({
+                count: 1, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    if (localIds && localIds.length === 1) {
+                        window.wx.uploadImage({
+                            localId: localIds[0], // 需要上传的图片的本地ID，由chooseImage接口获得
+                            isShowProgressTips: 1, // 默认为1，显示进度提示
+                            success: function (res) {
+                                var serverId = res.serverId; // 返回图片的服务器端ID
+                                this.handleDownLoadWecatImage(serverId);  //后台下载图片
+                            }.bind(this)
+                        });
+                    }
+                }.bind(this)
+            });
+        });
     }
 
     componentWillUnmount() {
         notification.remove('notification_2_start_record', this);
 
         notification.remove('notification_2_stop_record', this);
+
+        notification.remove('notification_2_upload_record', this);
+
+        notification.remove('notification_2_picture', this);
     }
 
     handleLoadKey() {
@@ -607,7 +633,7 @@ class Index extends Component {
                                                     }
                                                     {
                                                         this.props.key2.step3 === 1 ?
-                                                            <Record id="2" task_name={this.props.key2.task.task_name}/>
+                                                            <Picture id="2" task_name={this.props.key2.task.task_name}/>
                                                             :
                                                             ''
                                                     }
